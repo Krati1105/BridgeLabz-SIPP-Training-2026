574aa9eec67b80dc6dbc1c59a11b2b293fda90a5
Krati1105 <agrakrati123@gmail.com>
2026-06-09 12:21:40 +0530

    [KRATI AGRAWAL]Add subfolders level1,level2,level3 of gcr-codebase

---
+++ core-java-practice/gcr-codebase/control-flow/level2/FactorsWhile.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class FactorsWhile {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a positive integer: ");
+        int number = sc.nextInt();
+        if (number > 0) {
+            int counter = 1;
+            System.out.println("Factors:");
+            while (counter < number) {
+                if (number % counter == 0) {
+                    System.out.println(counter);
+                }
+                counter++;
+            }
+        } else {
+            System.out.println("Not a positive integer");
+        }
+        sc.close();
+    }
+}
---
+++ core-java-practice/gcr-codebase/control-flow/level2/MultipleBelowHundredFor.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class MultiplesBelowHundredFor {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a positive integer less than 100: ");
+        int number = sc.nextInt();
+        if (number > 0 && number < 100) {
+            System.out.println("Multiples below 100:");
+            for (int i = number; i < 100; i += number) {
+                System.out.println(i);
+            }
+        } else {
+            System.out.println("Invalid input");
+        }
+        sc.close();
+    }
+}
